ef1e0cf6c74572b790ac190e12500ad870cecc7e
uche-inyama <depoulet@yahoo.com>
2022-04-23 01:37:25 +0000

    category filter

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -10,11 +10,13 @@ class Discussion < ApplicationRecord
 
   accepts_nested_attributes_for :posts
 
+  broadcasts_to :category, inserts_by: :prepend
+
   after_create_commit -> { broadcast_prepend_to "discussions" }
   after_update_commit -> { broadcast_replace_to "discussions" }
   after_destroy_commit -> { broadcast_remove_to "discussions" }
 
-  # def to_param
-  #   "#{id}-#{name.downcase.to_s[0...100]}".parameterize
-  # end
+  def to_param
+    "#{id}-#{name.downcase.to_s[0...100]}".parameterize
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :discussions do
     resources :posts, only: [:create, :edit, :update, :destroy], module: :discussions
+
+    collection do
+      get 'category/:id', to: "categories/discussions#index", as: :category
+    end
   end
   root to: "main#index"
 end
